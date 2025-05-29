8085351e9b5139036c92991cf286b027bcb9be9a
sonasunny123 <sonasunny023@gmail.com>
2025-05-29 20:14:49 +0530

    Commit

---
+++ mavenproject/src/main/java/inheritance/SingleInheritanceChild.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class SingleInheritanceChild extends SingleInheritanceParent 
+{
+
+	public static void main(String[] args)
+	{
+		SingleInheritanceChild obj=new SingleInheritanceChild();
+		obj.print();
+	}
+
+}
---
+++ mavenproject/src/main/java/inheritance/SingleInheritanceParent.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class SingleInheritanceParent 
+{
+	public void print()
+	{
+		System.out.println("My name is Sona");
+	}
+}
